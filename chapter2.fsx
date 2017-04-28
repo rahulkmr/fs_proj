74fc22462032eace2eeea36aedcec5cb03995897
Rahul Kumar <rahul@thoughtnirvana.com>
2017-04-28 18:43:07 +0530

    Don't run code.
    Avoids delay while #load.

--- chapter2.fsx
+++ chapter2.fsx
@@ -40,23 +40,22 @@ let http (url: string) =
 let fetchBBC () = http "http://news.bbc.co.uk"
 
 
+
+
 type Species = HtmlProvider<"http://en.wikipedia.org/wiki/The_world's_100_most_threatened_species">
 
-let species =
-    [ for x in Species.GetSample().Tables.``Species list``.Rows ->
-        x.Type, x.``Common name``]
-
-let speciesSorted =
-    species
-        |> List.countBy fst
-        |> List.sortByDescending snd
-
-
-let html =
-    [ yield "<html><body><ul>"
-      for (category, count) in speciesSorted do
-        yield sprintf "<li>Category <b>%s</b>: <b>%d</b></li>" category count
-      yield "</ul></body></html>" ]
-    |> String.concat "\n"
-
-let startServer () = startWebServer defaultConfig (Successful.OK html)
+let startServer () =
+    let species =
+        [ for x in Species.GetSample().Tables.``Species list``.Rows ->
+            x.Type, x.``Common name``]
+    let speciesSorted =
+        species
+            |> List.countBy fst
+            |> List.sortByDescending snd
+    let html =
+        [ yield "<html><body><ul>"
+          for (category, count) in speciesSorted do
+            yield sprintf "<li>Category <b>%s</b>: <b>%d</b></li>" category count
+          yield "</ul></body></html>" ]
+        |> String.concat "\n"
+    startWebServer defaultConfig (Successful.OK html)
