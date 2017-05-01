dae75535f148f6f640c76de8aed9575a4cf9a928
Rahul Kumar <rahul@thoughtnirvana.com>
2017-05-01 10:58:27 +0530

    Don't download paket everytime

--- build.sh
+++ build.sh
@@ -18,11 +18,11 @@ then
   packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
 else
   # use mono
-  mono .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
+  # mono .paket/paket.bootstrapper.exe
+  # exit_code=$?
+  # if [ $exit_code -ne 0 ]; then
+  # 	exit $exit_code
+  # fi
 
   mono .paket/paket.exe restore
   exit_code=$?
